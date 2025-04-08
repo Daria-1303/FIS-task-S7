8fd42e9116f40c517be6d82e15b80e0b07f2078f
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-04-08 20:35:56 +0300

    Am ajuns la 2.5 task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,14 @@
  */
 package org.example;
 
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[]args){
+        AnsiConsole.systemInstall();
+        System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("Hello World!").reset());
+        AnsiConsole.systemUninstall();
     }
 }
