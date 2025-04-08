1824aa8346b5dcfc923e1fbdb0ec982d348d38d0
Daria-1303 <daria.ciobanu@student.upt.ro>
2025-04-08 20:46:06 +0300

    Finished gradle app

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,25 @@ import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
 public class Main {
-
-    public static void main(String[]args){
+    public static void main(String[] args) {
         AnsiConsole.systemInstall();
-        System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("Hello World!").reset());
+
+        String language;
+
+        if (args.length == 0) {
+            language = "en";
+        } else {
+            language = args[0].toLowerCase();
+        }
+        Greeting gp = new Greeting(language);
+
+        System.out.println(Ansi.ansi()
+                .fg(gp.getGreetingColor()).a(gp.getGreeting() + " ")
+                .fg(gp.getPersonColor()).a(gp.getPerson() + " ")
+                .fg(gp.getPunctuationColor()).a(gp.getPunctuation())
+                .reset()
+        );
+
         AnsiConsole.systemUninstall();
     }
 }
